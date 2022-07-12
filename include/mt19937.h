5397fbbb7c9ff8d834e7ade26b478261d531da76
Z Wilder <zwilder@wsl-blog.com>
2022-07-12 00:09:56 -0700

    Adding project to github
    Added license/readme files and boilerplate stuff.

--- include/mt19937.h
+++ include/mt19937.h
@@ -1,3 +1,22 @@
+/*
+* Cards
+* Copyright (C) Zach Wilder 2022
+* 
+* This file is a part of Cards
+*
+* Cards is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* Cards is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with Cards.  If not, see <http://www.gnu.org/licenses/>.
+*/
 /* 
    A C-program for MT19937, with initialization improved 2002/1/26.
    Coded by Takuji Nishimura and Makoto Matsumoto.
---
+++ readme.md
@@ -0,0 +1,4 @@
+##Cards
+
+An overdesigned deck of cards with the hope of adding a bunch of card
+games at some point. 
